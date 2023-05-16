8713c694f2164ebc688b7bb204a04d627be617b9
princy jogani <joganiprincy@gmail.com>
2023-05-16 12:58:33 +0530

    first commit

---
+++ Q1_1.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+clrscr();
+
+printf("jogani princy\n");
+printf("age =17\n");
+printf("ashaeep\n");
+getch();
+
+}
---
+++ Q2.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+clrscr();
+printf("-------\n");
+printf("|\t|\n");
+printf("r\t|\n");
+printf("n\t|\n");
+printf("w\t|\n");
+printf("|\t|\n");
+printf("-------\n");
+getch();
+}
